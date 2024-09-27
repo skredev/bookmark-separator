a20063f70b9a3476c9f602897dceb284371cfe71
Samuel Krebs <hi@skre.dev>
2024-09-27 22:25:04 +0200

    perf: format code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,23 @@
-window.$_GET = location.search.substr(1).split("&")  // GET parameter
+window.$_GET = location.search.substr(1).split("&"); // GET parameter
 
-const headEl = document.getElementById("head-el")
+const headEl = document.getElementById("head-el");
 
-const favicon = document.createElement("link") // Creation of the favicon element
-favicon.setAttribute('rel', 'icon')
-
+const favicon = document.createElement("link"); // Creation of the favicon element
+favicon.setAttribute("rel", "icon");
 
 // Setting the favicon according to the requested orientation
-if(window.$_GET[0] === "horizontal"){
-    favicon.setAttribute('type', 'image/svg+xml')
-    favicon.setAttribute('sizes', 'any')
-    favicon.setAttribute('href', './assets/images/horizontal.svg')
-    headEl.append(favicon)
-}else if(window.$_GET[0] === "vertical"){
-    favicon.setAttribute('type', 'image/svg+xml')
-    favicon.setAttribute('sizes', 'any')
-    favicon.setAttribute('href', './assets/images/vertical.svg')
-    headEl.append(favicon)
-}else{
-    favicon.setAttribute('type', 'image/x-icon')
-    favicon.setAttribute('href', './assets/icons/favicon.ico')
-    headEl.append(favicon)
+if (window.$_GET[0] === "horizontal") {
+  favicon.setAttribute("type", "image/svg+xml");
+  favicon.setAttribute("sizes", "any");
+  favicon.setAttribute("href", "./assets/images/horizontal.svg");
+  headEl.append(favicon);
+} else if (window.$_GET[0] === "vertical") {
+  favicon.setAttribute("type", "image/svg+xml");
+  favicon.setAttribute("sizes", "any");
+  favicon.setAttribute("href", "./assets/images/vertical.svg");
+  headEl.append(favicon);
+} else {
+  favicon.setAttribute("type", "image/x-icon");
+  favicon.setAttribute("href", "./assets/icons/favicon.ico");
+  headEl.append(favicon);
 }
